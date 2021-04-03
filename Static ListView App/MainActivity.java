a5affe5682b2ed48f9c3f3aa6491fcade69a72e8
Umar Farooq <80045035+umareefarooq@users.noreply.github.com>
2021-04-03 10:31:51 -0700

    added static listview app

---
+++ Static ListView App/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.listviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}

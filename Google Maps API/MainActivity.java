eaec65748a7fd708e9d76fe2bb77cb571725f155
Umar Farooq <80045035+umareefarooq@users.noreply.github.com>
2021-06-12 15:26:38 +0500

    added google maps code files
    The google maps API is used in this app. The app has the lats and longs on my friend's house. The marker shows a title.
    Also the map has a hybrid satilite image view.

---
+++ Google Maps API/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.googlemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(i);
+    }
+}
